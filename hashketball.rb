f809777a41f84b2a9c1b8735e1e984ba23ab89f3
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-12 01:56:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,8 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [
         {
-          :player_name => "Alan Anderson", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0}
+          :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
+          :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}
         ]
     },
     :away => {
