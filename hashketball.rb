4ae0fc6f788e807da1558163f9181bd696036c57
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-12 02:03:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,9 +26,9 @@ def game_hash
           
           {:player_name => "Brook Lopez", :number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 15},
           
-          {:player_name => "Mason Plumlee", :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
+          {:player_name => "Mason Plumlee", :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 3, :blocks => 8, :slam_dunks => 5},
           
-          {:player_name => "Jason Terry", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
+          {:player_name => "Jason Terry", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 11, :slam_dunks => 1}
         ]
     }
   }
