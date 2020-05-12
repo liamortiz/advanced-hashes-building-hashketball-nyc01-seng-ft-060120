60ff34472caf34a4b9b2b00477ed42949229968f
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-12 01:55:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,30 +5,16 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {:player_name => "Alan Anderson"}, 
-        {:number => 0},
-        {:shoe => 0},
-        {:points => 0},
-        {:rebounds => 0},
-        {:assists => 0},
-        {:steals => 0},
-        {:blocks => 0},
-        {:slam_dunks => 0}
+        {
+          :player_name => "Jeff Adrien", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0}
         ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
-        {:player_name => "Jeff Adrien"}, 
-        {:number => 0},
-        {:shoe => 0},
-        {:points => 0},
-        {:rebounds => 0},
-        {:assists => 0},
-        {:steals => 0},
-        {:blocks => 0},
-        {:slam_dunks => 0}
+        {
+          :player_name => "Jeff Adrien", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0}
         ]
     }
   }
