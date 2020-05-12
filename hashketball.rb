4e2554e575f7efecb253f33127ae977b4ba27460
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-12 01:48:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,10 +4,7 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [
-        {:player_name => ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee"]}, 
-        {:number => [16, 14, 17, ]}
-        ]
+      :players => []
     },
     :away => {
       :team_name => "",
