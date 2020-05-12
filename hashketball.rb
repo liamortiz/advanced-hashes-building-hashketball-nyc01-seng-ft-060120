8ec47f0328dd533c0568e6c5e144529a15db1704
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-12 02:04:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,15 +20,15 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
-        {:player_name => "Alan Anderson", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 3, :blocks => 1, :slam_dunks => 1},
+        {:player_name => "Alan Anderson", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
           
-          {:player_name => "Reggie Evans", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 12, :blocks => 12, :slam_dunks => 7},
+          {:player_name => "Reggie Evans", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10},
           
-          {:player_name => "Brook Lopez", :number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 15},
+          {:player_name => "Brook Lopez", :number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
           
-          {:player_name => "Mason Plumlee", :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 3, :blocks => 8, :slam_dunks => 5},
+          {:player_name => "Mason Plumlee", :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
           
-          {:player_name => "Jason Terry", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 11, :slam_dunks => 1}
+          {:player_name => "Jason Terry", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_dunks => 12}
         ]
     }
   }
