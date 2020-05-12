2ed0d053c03e1f77d24ca4fa3d1d862e7b124d5f
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-12 02:04:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,7 +22,7 @@ def game_hash
       :players => [
         {:player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
           
-          {:player_name => "Reggie Evans", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10},
+          {:player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10},
           
           {:player_name => "Brook Lopez", :number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
           
